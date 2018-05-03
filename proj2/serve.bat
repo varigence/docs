dc9f3df03bfa3d9c7c6e365b9c61b546122faff9
Stefan Johansson <stefan.johansson@varigence.com.au>
2018-05-03 13:25:04 +1000

    wip update to proj2 for metadat gen and build

--- proj2/serve.bat
+++ proj2/serve.bat
@@ -1,1 +1,2 @@
+docfx metadata docfx.json
 docfx build docfx.json -t _exported_templates\default\ --serve
---
+++ proj2/testapp/testapp/testclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testapp
+{
+ /// <summary>
+ /// test documentation
+ /// </summary>
+public class testclass
+    {
+        /// <summary>
+        ///test ctor doco
+        /// </summary>
+        public testclass()
+        {
+            int i = 1 + 1;
+        }
+    }
+}
