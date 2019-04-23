b1f67c90c599d72b30984af79095a4501d66a589
Michael Buller <michaelbuller@gmail.com>
2019-04-23 15:22:05 -0400

    pushing api reference template cleanup

--- docfx/serve.bat
+++ docfx/serve.bat
@@ -1,3 +1,3 @@
-docfx metadata docfx.json
+C:\docfxsrc\docfx\src\docfx\bin\Debug\net462\docfx metadata docfx.json
 rem docfx build docfx.json -t varigence-template-html\ --serve
-docfx build docfx.json --serve
+C:\docfxsrc\docfx\src\docfx\bin\Debug\net462\docfx build docfx.json --serve
