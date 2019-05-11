6e3f96a8cc8c8f45d1de8ebc4bdbeb67bd5abdf9
scottcurrie <scottcurrie@users.noreply.github.com>
2019-05-11 15:46:00 -0400

    Using custom docfx tooling for now

---
+++ docfx/build.bat
@@ -0,0 +1,4 @@
+tools\docfx metadata docfxWebOnly.json
+tools\docfx build docfxWebOnly.json 
+node templates\varigence-template-html\styles\build-index.js
+
--- docfx/serve.bat
+++ docfx/serve.bat
@@ -1,5 +1,4 @@
-C:\docfxsrc\docfx\src\docfx\bin\Debug\net462\docfx metadata docfx.json
-rem docfx build docfx.json -t varigence-template-html\ --serve
-C:\docfxsrc\docfx\src\docfx\bin\Debug\net462\docfx build docfx.json 
+tools\docfx metadata docfx.json
+tools\docfx build docfx.json 
 node templates\varigence-template-html\styles\build-index.js
-C:\docfxsrc\docfx\src\docfx\bin\Debug\net462\docfx serve _site
+tools\docfx serve _site
