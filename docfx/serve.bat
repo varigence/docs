d1cd6416efcd9119abc33909b2e7e90a3c41dd62
Enrico Saunders <esa@varigence.com>
2020-04-02 16:53:01 +1100

    Revert checkin of serve.bat

--- docfx/serve.bat
+++ docfx/serve.bat
@@ -1,4 +1,4 @@
-REM tools\docfx metadata docfxWebOnly.json
+tools\docfx metadata docfxWebOnly.json
 tools\docfx build docfxWebOnly.json 
-REM node templates\varigence-template-html\styles\build-index.js
+node templates\varigence-template-html\styles\build-index.js
 tools\docfx serve _site
