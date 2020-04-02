9085d97c0212f6ad36ee66e353985efb44d12072
Enrico Saunders <esa@varigence.com>
2020-04-02 11:09:44 +1100

    Responsive styling wip, hide navbar, remove sub menu, container width

--- docfx/serve.bat
+++ docfx/serve.bat
@@ -1,4 +1,4 @@
-tools\docfx metadata docfxWebOnly.json
+REM tools\docfx metadata docfxWebOnly.json
 tools\docfx build docfxWebOnly.json 
-node templates\varigence-template-html\styles\build-index.js
+REM node templates\varigence-template-html\styles\build-index.js
 tools\docfx serve _site
