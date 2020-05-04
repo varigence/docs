fd825a60f64baef2b0d3f136d6fa4bd6f032197f
avenantp <pav@varigence.com>
2020-05-04 16:38:01 +1000

    Remove old images

--- bimlflex/user-guide/images/_resize_images.cmd
+++
@@ -1,8 +0,0 @@
-@echo on
-pushd %~dp0
-set "source_folder=%~dp0"
-set "result_folder_1=%~dp0\resize"
-
-for %%a in ("*.png") do (
-   call _script_resize.cmd -source "%%~fa" -target "resize\%%~nxa" -max-width 250 -keep-ratio yes -force yes
-)
