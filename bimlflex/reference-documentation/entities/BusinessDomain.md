98925bdfd56081b7aeee427c373d4e87892e48ca
avenantp <pav@varigence.com>
2023-12-08 07:34:56 +1100

    latest docusaurus migration

---
+++ bimlflex/reference-documentation/entities/BusinessDomain.md
@@ -0,0 +1,27 @@
+---
+uid: bimlflex-app-reference-documentation-BusinessDomains
+title: BimlFlex App Definition for BusinessDomains
+summary: Documentation of functionality within BimlFlex for BusinessDomains
+varigenceProduct: BimlFlex
+varigenceArticleType: Reference
+---
+
+# BusinessDomains
+
+
+
+## Overview
+  
+| <div style="width:200px">Property</div> | Description |
+| --------- | ----------- |
+|Name | |
+|Industry | |
+|Description | |
+
+## References
+  
+| <div style="width:200px">Property</div> | Description |
+| --------- | ----------- |
+|CustomerUID | |
+|VersionUID | |
+
--- utilities/MigrateDocfx/Program.cs
+++ utilities/MigrateDocfx/Program.cs
@@ -45,7 +45,7 @@ static void MigrateFile(string filePath, string imageRegex, string docsFolder, s
     var newPath = filePath.Replace(docFxDirectory, docusaurusDirectory);
     var content = File.ReadAllText(filePath);
 
-    foreach (Match match in new Regex(@"(!\[.*?\]\()(https:.*?)(\))", RegexOptions.IgnoreCase).Matches(content))
+    foreach (Match match in new Regex(@"(!\[.*?\]\()(https:.*?)((\W"".*?\))|\))", RegexOptions.IgnoreCase).Matches(content))
     {
         if (match.Success)
         {
@@ -59,7 +59,7 @@ static void MigrateFile(string filePath, string imageRegex, string docsFolder, s
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("ImageUrl Missing: " + imageUrl);
             }
         }
     }
@@ -311,29 +311,37 @@ static void RenameFile()
 
 static void SaveImageFromUrl(string file_name, string url)
 {
-    byte[] content;
-    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-    WebResponse response = request.GetResponse();
-
-    Stream stream = response.GetResponseStream();
-
-    using (BinaryReader br = new BinaryReader(stream))
-    {
-        content = br.ReadBytes(500000);
-        br.Close();
-    }
-    response.Close();
-
-    FileStream fs = new FileStream(file_name, FileMode.Create);
-    BinaryWriter bw = new BinaryWriter(fs);
     try
     {
-        bw.Write(content);
+        byte[] content;
+        var request = (HttpWebRequest)WebRequest.Create(url);
+        var response = request.GetResponse();
+
+        var stream = response.GetResponseStream();
+
+        using (BinaryReader br = new BinaryReader(stream))
+        {
+            content = br.ReadBytes(500000);
+            br.Close();
+        }
+        response.Close();
+
+        var fs = new FileStream(file_name, FileMode.Create);
+        var bw = new BinaryWriter(fs);
+        try
+        {
+            bw.Write(content);
+        }
+        finally
+        {
+            fs.Close();
+            bw.Close();
+        }
     }
-    finally
+    catch (Exception ex)
     {
-        fs.Close();
-        bw.Close();
+        Console.WriteLine($"#{url}#");
+        Console.WriteLine(ex.Message);
     }
 }
 public class TocEntry
