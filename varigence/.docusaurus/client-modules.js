39546e7df8dbe9827f6ad3301ea5c503539d50b4
Scott Currie <scott.currie@varigence.com>
2023-12-12 12:30:02 -0500

    Removing docusaurus build folder

--- varigence/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\varigencedocs\\varigence\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\varigencedocs\\varigence\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\varigencedocs\\varigence\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\varigencedocs\\varigence\\src\\css\\custom.css"),
-];
